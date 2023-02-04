b73bcab8c2b431f3e16ed70ac4b4e44f7cac5e73
Roman Radionov <roman.s.radionov@gmail.com>
2023-02-04 23:36:29 +0200

    mini-project

--- mini-project/main.js
+++ mini-project/main.js
@@ -30,6 +30,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
 // 3 Додати кожному блоку кнопку/посилання,
 //   при кліку на яку відбувається перехід на сторінку user-details.html,
 //   котра має детальну інформацію про об'єкт на який клікнули
+let modal = document.getElementById('modal-active');
 function getUserDetails (id) {
     fetch(`https://jsonplaceholder.typicode.com/users/`)
         .then(response => response.json())
@@ -37,13 +38,37 @@ function getUserDetails (id) {
             for ( const item of value){
                 console.log(item)
                 if (id == item.id){
-                    let modal = document.getElementById('modal-active');
                     modal.innerHTML = `
-                    <div class="modal-main active" id="modal__delete">
-                        <div class="modal-main__content" onclick="closeModalDeleteWindow()">
-                            <button class="modal-main__close" id="delete" onclick="closeModalDeleteWindow()">&times;</button>
-                            <h3 class="modal-main__title">${item.id}) ${item.name}</h3>
-                            <p>${item.name}</p>
+                    <div class="modal">
+                        <div class="modal-main" onclick="closeModalDeleteWindow()">
+                            <button class="modal-main-close" id="delete" onclick="closeModalDeleteWindow()">&times;</button>
+                            <div class="modal-item">
+                                <p class="modal-main-title">${item.id}) ${item.name}</p>
+                            </div>
+                            <div class="modal-item">
+                                <i class="fa-regular fa-id-card"></i>
+                                <p>nickname: ${item.username}</p>
+                            </div>
+                            <div class="modal-item">
+                                <i class="fa-solid fa-envelope"></i>
+                                <p>email: ${item.email}</p>
+                            </div>
+                            <div class="modal-item">
+                                <i class="fa-solid fa-earth-americas"></i>
+                                <p>address: ${item.address.city}</p>
+                            </div>
+                            <div class="modal-item">
+                                <i class="fa-solid fa-phone-flip"></i>
+                                <p>phone: ${item.phone}</p>
+                            </div>
+                            <div class="modal-item">
+                                <i class="fa-regular fa-id-card"></i>
+                                <p>site: ${item.website}</p>
+                            </div>
+                            <div class="modal-item">
+                                <i class="fa-regular fa-id-card"></i>
+                                <p>company: ${item.company.name}</p>
+                            </div>
                         </div>
                     </div>
                     `;
@@ -52,3 +77,20 @@ function getUserDetails (id) {
             }
         })
 }
+
+// На странице user-details.html:
+// 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
+// 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
+// (для получения постів используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+//     6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
+//
+//     На странице post-details.html:
+// 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
+// 8 Нижчє інформацію про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+//
+// Стилізація проєкта -
+// index.html - всі блоки з user - по 2 в рядок. кнопки/посилвння розташувати під інформацією про user.
+//     user-details.html - блок з інфою про user зверху сторінки. Кнопка нижчє, на 90% ширини сторінки, по центру.
+//     блоки з короткою іфною про post - в ряд по 5 .
+//     post-details.html - блок з інфою про пост зверху. Коментарі - по 4 в ряд.
+//     Всі елементи котрі характеризують users, posts, comments візуалізувати, так, щоб було видно що це блоки (дати фон. марджини і тд)
