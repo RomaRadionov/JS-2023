9052cd33cc711355e135cab7b4a726636eeb0101
Roman Radionov <roman.s.radionov@gmail.com>
2023-02-26 22:05:37 +0200

    mini-project not done

--- mini-project/main.js
+++ mini-project/main.js
@@ -38,7 +38,7 @@ function getUserDetails (id) {
         .then(response => response.json())
         .then(value => {
             for ( const item of value){
-                console.log(item)
+                // console.log(item)
                 if (id === item.id){
                     modal.innerHTML = `
                     <div class="modal">
@@ -83,28 +83,56 @@ function getUserDetails (id) {
 
 
 // Пишемо функцію яка буде закривати модальне вікно
-const closeModalDeleteWindow = () => {modal.innerHTML = ''};
+const closeModalDeleteWindow = () => {
+    modal.innerHTML = '';
+    posts.innerHTML = ''
+};
 
 
 // Пишемо функцію яка буде відкривати модальне вікно з постами даного юзера
+let posts = document.createElement('div');
 function getUserPosts (id) {
+    modal.innerHTML = '';
     fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
         .then(response => response.json())
         .then(value => {
-            modal.innerHTML = ``;
-            for (const post of value) {
-                console.log(post);
-                modal.innerHTML += `
-                <div class="modal">
-                    <div class="modal-main">
-                            <button class="modal-main-close" id="delete" onclick="closeModalDeleteWindow()"><i class="fa-solid fa-chevron-left"></i></button>
+            modal.append(posts);
+            posts.classList.add('grid', 'post');
+            posts.innerHTML =
+                '<button class="modal-main-close" id="delete" onclick="closeModalDeleteWindow()"><i class="fa-solid fa-chevron-left"></i></button>';
+            for (const postItem of value) {
+                // console.log(postItem);
+                posts.innerHTML += `
+                    <div class="modal-main">                            
                             <div class="modal-item">
                                 <i class="fa-regular fa-id-card"></i>
-                                <p>Post: ${post.id}</p>
-                                <p>Title: ${post.title}</p>                                
+                                <p>Post: ${postItem.id}</p>
+                                <p>Title: ${postItem.title}</p>                                
                             </div>
+                            <button class="btn" onclick="getUserPostsInfo(${postItem.id})" id="${postItem.id}">More...</button>
                     </div>
-                </div>
+                `;
+            }
+        })
+}
+
+function getUserPostsInfo(id) {
+    posts.innerHTML = '';
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        .then(response => response.json())
+        .then(value => {
+            posts.innerHTML = `
+                <button class="modal-main-close" id="delete" onclick="closeModalDeleteWindow()"><i class="fa-solid fa-chevron-left"></i></button>`;
+            for ( const post of value) {
+                // console.log(post);
+                posts.innerHTML += `
+                    <div class="modal-coment">
+                         <i class="fa-regular fa-id-card"></i>
+                         <p>Post: ${post.id}</p>
+                         <p>name: ${post.name}</p>                                
+                         <p>email: ${post.email}</p>                                
+                         <p>body: ${post.body}</p>                                
+                    </div>
                 `;
             }
         })
@@ -114,10 +142,10 @@ function getUserPosts (id) {
 // 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
 // 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
 // (для отримання постів використовуйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
-//     6 Кожному посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html,
-//     котра має детальну інфу про поточний пост.
+// 6 Кожному посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html,
+//   котра має детальну інфу про поточний пост.
 //
-//     На сторінці post-details.html:
+// На сторінці post-details.html:
 // 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули.
 // 8 Нижче інформацію про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 //
